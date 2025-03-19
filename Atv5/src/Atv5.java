a3fe2e226d4c07edcf58f75af1c2284a1ba24a59
caduud <cadugnatcc@gmail.com>
2025-03-19 16:29:53 -0300

    Atividade completa

---
+++ Atv5/src/Atv5.java
@@ -0,0 +1,10 @@
+public class Atv5 {
+    /* Classe = JAVA
+        Metodo = getNome()
+        Objeto = javaObj
+
+        Classe = É o modelo que define os atributos e os métodos para os objetos
+        Metodo = Uma função feita dentro de uma classe chamada para realizar alguma ação em um código executavel
+        Objeto = Instância de uma classe que possui atributos e métodos
+     */
+}
