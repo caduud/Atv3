e2f5b18159e6eb6e6efb75f9e3fa1d509c8acdd9
caduud <cadugnatcc@gmail.com>
2025-03-19 16:38:30 -0300

    atividade completa

---
+++ Atv6/src/Conta.java
@@ -0,0 +1,26 @@
+public class Conta {
+    private double saldo;
+    public Conta (double saldoInicial) {
+        if (saldoInicial > 0.0)
+            saldo = saldoInicial;
+        else
+            saldo = 0.0;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void creditar (double quantia) {
+        saldo += quantia;
+    }
+    public void debitar (double quantia) {
+        if (quantia > this.saldo){
+            System.out.println("Quantia de débito excedeu o saldo da conta");
+        }else{
+            saldo -= quantia;
+        }
+
+    }
+
+}
+
+
