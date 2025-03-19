206bc87e678d8f13f09c49a5d6d1e29a78283c74
caduud <cadugnatcc@gmail.com>
2025-03-19 16:07:37 -0300

    Nova atividade

---
+++ Atv4/src/Teste.java
@@ -0,0 +1,10 @@
+public class Teste {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro("Harry Potter", "J.K Rollhing");
+        Livro livro2 = new Livro("O pequeno príncipe", "Antonie de Saint");
+
+        livro1.exibeDados();
+        livro2.exibeDados();
+
+    }
+}
